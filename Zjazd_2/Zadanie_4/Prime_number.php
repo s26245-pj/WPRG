6b4656287cc00633bb121617a20e0d7d40e548c3
s26245-pj <s26245@pjwstk.edu.pl>
2023-04-12 19:11:18 +0200

    Prime number exercise finished

--- Zjazd_2/Zadanie_4/Prime_number.php
+++ Zjazd_2/Zadanie_4/Prime_number.php
@@ -1,11 +1,12 @@
 <?php
 
+
+$count = 0;
 function isPrime($number) {
-    $count = 0;
+    global $count;
     if($number < 2) {
         return false;
     }
-
     for ($i = 2; $i * $i <= $number; $i++) {
         if ($number % $i == 0) {
             return false;
@@ -19,11 +20,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $number = $_POST["number"];
     if (is_numeric($number) && intval($number) == $number && $number > 0) {
     $number = intval($number);
-    $iterations = 0;
     $result = isPrime($number);
     echo "The number $number " . ($result ? "is" : "is not") . " a prime number.<br>";
-    echo "Number of iterations: $iterations";
+    echo "Number of iterations: $count";
     } else {
-    echo "Invalid input. Please enter a positive integer.";
+    echo "Invalid input. Please enter a positive number.";
     }
 }
